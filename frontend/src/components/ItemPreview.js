c09da4d0681dfa63af230fcdc9b07830930dfa19
Chris Eich <cjeich@gmail.com>
2022-10-11 21:55:55 -0700

    CE - Add placeholder to Item List View

--- frontend/src/components/ItemPreview.js
+++ frontend/src/components/ItemPreview.js
@@ -29,6 +29,8 @@ const ItemPreview = (props) => {
     }
   };
 
+  const DEFUALT_IMG_URL = "/placeholder.png"
+
   return (
     <div
       className="card bg-dark border-light p-3"
@@ -36,7 +38,7 @@ const ItemPreview = (props) => {
     >
       <img
         alt="item"
-        src={item.image}
+        src={item.image || DEFUALT_IMG_URL}
         className="card-img-top item-img"
         style={{ borderRadius: "20px" }}
       />
