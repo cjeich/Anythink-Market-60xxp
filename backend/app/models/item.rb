96e04e33efd0d1edd70f22370fbc044d022c3139
Chris Eich <cjeich@gmail.com>
2022-10-13 22:14:17 -0700

    CE - Update Image Method
    -- Also added comments to show DB info for rails model

--- backend/app/models/item.rb
+++ backend/app/models/item.rb
@@ -1,5 +1,20 @@
 # frozen_string_literal: true
 
+# == Schema Information
+#
+# Table name: items
+#
+#  id              :integer          not null, primary key
+#  title           :string
+#  slug            :string
+#  description     :string
+#  image           :string
+#  favorites_count :integer
+#  user_id         :integer
+#  created_at      :datetime         not null
+#  updated_at      :datetime         not null
+#
+
 class Item < ApplicationRecord
   belongs_to :user
   has_many :favorites, dependent: :destroy
@@ -18,7 +33,7 @@ class Item < ApplicationRecord
     self.slug ||= "#{title.to_s.parameterize}-#{rand(36**6).to_s(36)}"
   end
 
-  def image_url
+  def image
     super || "/placeholder.png"
   end
 end
